455500e6ee692b6d60f20c6f047538c2d3ccfe6d
Henry Riddall <henryriddall@gmail.com>
2021-07-05 12:00:41 +0100

    Implemented Pawns moving one square

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from "../square";
+import Player from "../player";
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,12 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this)
+        if (this.player == Player.WHITE) {
+            return new Array(new Square(currentSquare.row+1, currentSquare.col));
+        }
+        else {
+            return new Array(new Square(currentSquare.row-1, currentSquare.col));
+        }
     }
 }
